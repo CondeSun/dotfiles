123a22d1a27b3665a2b197a865383032cd9eb1fb
Michael Fritsch <michael.fritsch@bayformatic.de>
2026-02-22 13:15:33 +0100

    added nvim configuration

---
+++ .config/nvim/lua/plugins/svelte.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "svelte" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svelte = {
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      if LazyVim.has_extra("formatting.prettier") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.svelte = { "prettier" }
+      end
+    end,
+  },
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# dotfiles
+# My dotfiles
+
+## Components
+
+- NVIM (.config/nvim)
+- EMACS (.emacs.d)
+
+## How to configure
+
+1. Clone this Repository to any local directory
+2. Create a Symlink to your actual home directory
+
+### Appendix Symlink
+
+```bash
+ln -s $HOME/Documents/DevProjects/dotfiles/.emacs.d $HOME/.emacs.d
+ln -s $HOME/Documents/DevProjects/dotfiles/.config/nvim $HOME/.config/nvim
+```
