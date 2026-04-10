5b8177c706281ad0c4f52b2c81fc06b3230d5237
Michael Fritsch <michael.fritsch@bayformatic.de>
2026-04-10 16:28:18 +0200

    added lsp keymapping

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -74,3 +74,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; map lsp error jumps to code context
+(map! :leader
+      "c n" #'next-error
+      "c p" #'previous-error)
